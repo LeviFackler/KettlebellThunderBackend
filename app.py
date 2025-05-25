0ed331356df452f74efe6ed5eb561bdbc4c68b3a
Levi <levi.fackler@gmail.com>
2025-05-25 10:07:54 -0400

    Corrected app.py import modules

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
